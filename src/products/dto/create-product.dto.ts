08453cb2b115b057b50ff4b108d92d32b28fad96
Luciano Figueroa <lumusitech@gmail.com>
2025-07-09 13:02:34 -0300

    feat: add create product dto with validations

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title!: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'unisex', 'kid'])
+  gender: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+}
