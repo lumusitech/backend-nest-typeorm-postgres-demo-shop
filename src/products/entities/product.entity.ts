bb6b2038723802c8823b22c9c7ace58aaf64b8e4
Luciano Figueroa <lumusitech@gmail.com>
2025-07-08 22:09:31 -0300

    chore: config typeorm for products feature

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,30 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text', unique: true })
+  title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'text', unique: true })
+  slug: string;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column({ type: 'text' })
+  gender: string;
+
+  // TODO: add tags and images props
+}
