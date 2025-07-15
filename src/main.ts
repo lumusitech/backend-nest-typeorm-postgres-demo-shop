c0aff28869d347d5acac155a7536122b7cfce5f5
Luciano Figueroa <lumusitech@gmail.com>
2025-07-14 22:08:54 -0300

    feat: add base swager docs config within main file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,7 +15,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo Shop API')
+    .setDescription('A Demo Shop API')
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`App running on port ${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
